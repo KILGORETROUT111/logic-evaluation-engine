e4de8013435f438755aa7a937e6edc911757fed1
KILGORETROUT111 <kilgoretrout@berkeley.edu>
2025-06-30 17:26:59 +0200

    Update state.py

--- core/state.py
+++ core/state.py
@@ -4,7 +4,7 @@
 #You should have received a copy of the GNU General Public License along with Logic Evaluation Engine.
 #If not, see <https://www.gnu.org/licenses/>.
 
-#state.py
+#
 
 
 from enum import Enum
