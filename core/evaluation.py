ebeaa8c061d6db45435905758998ae2615232406
KILGORETROUT111 <kilgoretrout@berkeley.edu>
2025-06-30 17:31:08 +0200

    Update evaluation.py

--- core/evaluation.py
+++ core/evaluation.py
@@ -4,6 +4,8 @@
 #You should have received a copy of the GNU General Public License along with Logic Evaluation Engine.
 #If not, see <https://www.gnu.org/licenses/>.
 
+#
+
 from core.expressions import Functor, Value, Var, State
 
 def evaluate_full(expr, context=None):
